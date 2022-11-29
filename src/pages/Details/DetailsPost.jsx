d87fcb24848a757da533c8d8d8ec6cd494bfab9e
TinWinHlaing-777 <tinwinhlaingmm777@gmail.com>
2022-11-29 19:06:37 +0630

    updated features

--- src/pages/Details/DetailsPost.jsx
+++ src/pages/Details/DetailsPost.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect } from "react";
-import "./detailPost.css";
-import Navbar from "../../components/Navbar/Navbar";
-import { AiFillLike, AiFillDislike } from "react-icons/ai";
-import { useParams } from "react-router";
-import axios from "axios";
-import { API_URL, MAIN_URL } from "../../config";
-import { getLocalData } from "../../utils/common";
+import React, { useEffect } from 'react';
+import './detailPost.css';
+import Navbar from '../../components/Navbar/Navbar';
+import { AiFillLike, AiFillDislike } from 'react-icons/ai';
+import { useParams } from 'react-router';
+import axios from 'axios';
+import { API_URL, MAIN_URL } from '../../config';
+import { getLocalData } from '../../utils/common';
 
 const DetailsPost = () => {
   const [detailInfo, setDetailInfo] = React.useState([]);
   const [like, setLike] = React.useState(false);
-  const [comment, setComment] = React.useState("");
+  const [comment, setComment] = React.useState('');
+  const [commentInfo, setCommentInfo] = React.useState([]);
 
   const { id } = useParams();
   const userId = getLocalData().getId;
@@ -20,13 +21,13 @@ const DetailsPost = () => {
     try {
       await axios
         .get(`${API_URL}/post/detail/${id}`)
-        .then((res) => {
-          res.data.forEach((data) => {
+        .then(res => {
+          res.data.forEach(data => {
             detailList.push(data);
             setDetailInfo(detailList);
           });
         })
-        .catch((err) => {
+        .catch(err => {
           console.log(err);
         });
     } catch (error) {
@@ -36,6 +37,7 @@ const DetailsPost = () => {
 
   useEffect(() => {
     showDetails();
+    getComment();
   }, []);
 
   const addLike = async () => {
@@ -46,10 +48,10 @@ const DetailsPost = () => {
       };
       await axios
         .patch(`${API_URL}/post/update/like/${id}`, likeData)
-        .then((res) => {
+        .then(res => {
           showDetails();
         })
-        .catch((err) => {
+        .catch(err => {
           console.log(err);
         });
     } catch (error) {
@@ -65,10 +67,10 @@ const DetailsPost = () => {
       };
       await axios
         .patch(`${API_URL}/post/update/dislike/${id}`, dislikeData)
-        .then((res) => {
+        .then(res => {
           showDetails();
         })
-        .catch((err) => {
+        .catch(err => {
           console.log(err);
         });
     } catch (error) {
@@ -76,8 +78,8 @@ const DetailsPost = () => {
     }
   };
 
-  const createComment = async (e) => {
-    if (e.key === "Enter") {
+  const createComment = async e => {
+    if (e.key === 'Enter') {
       try {
         const commentData = {
           comment: comment,
@@ -86,15 +88,36 @@ const DetailsPost = () => {
         };
         await axios
           .post(`${API_URL}/comment/create`, commentData)
-          .then((res) => {
+          .then(res => {
             if (res.status === 201) {
-              setComment("");
+              setComment('');
+              getComment();
             }
           })
-          .catch((err) => {
+          .catch(err => {
             console.log(err);
           });
-      } catch (error) {}
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const getComment = async () => {
+    let commentData = [];
+    try {
+      await axios
+        .get(`${API_URL}/comment/getbypost/${id}`)
+        .then(res => {
+          res.data.forEach(data => {
+            console.log(data);
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -103,86 +126,86 @@ const DetailsPost = () => {
       <Navbar />
       {detailInfo.map((element, index) => {
         return (
-          <div className="detailPost__container" key={index}>
-            <div className="detailPost__image">
+          <div className='detailPost__container' key={index}>
+            <div className='detailPost__image'>
               <img
-                crossOrigin="anonymous"
+                crossOrigin='anonymous'
                 src={`${MAIN_URL}/${element.postImage}`}
-                alt="smaple"
-                className="detail__image"
+                alt='smaple'
+                className='detail__image'
               />
             </div>
-            <div className="detailPost__content">
-              <h1 className="detailPost__header">{element.post_title}</h1>
-              <p className="detailPost__body">{element.post_body}</p>
+            <div className='detailPost__content'>
+              <h1 className='detailPost__header'>{element.post_title}</h1>
+              <p className='detailPost__body'>{element.post_body}</p>
             </div>
-            <div className="detailPost__comment__container">
+            <div className='detailPost__comment__container'>
               {like === false ? (
-                <button className="btn__like" onClick={() => addLike()}>
+                <button className='btn__like' onClick={() => addLike()}>
                   <AiFillLike />
                   Like
                 </button>
               ) : (
-                <button className="btn__like" onClick={() => removeLikes()}>
+                <button className='btn__like' onClick={() => removeLikes()}>
                   <AiFillDislike />
                   Dislike
                 </button>
               )}
               <input
-                type="text"
-                name="commetn"
-                id="comment"
-                className="comment__container"
-                placeholder="Write a comment"
+                type='text'
+                name='commetn'
+                id='comment'
+                className='comment__container'
+                placeholder='Write a comment'
                 value={comment}
-                onChange={(e) => setComment(e.target.value)}
+                onChange={e => setComment(e.target.value)}
                 onKeyDown={createComment}
               />
             </div>
-            <div className="detail__comment">
-              <p className="show__like">
+            <div className='detail__comment'>
+              <p className='show__like'>
                 {element.likeCount.length} like this post
               </p>
-              <div className="show__comment__container">
+              <div className='show__comment__container'>
                 <img
-                  src="/images/profile.png"
-                  alt="profile"
-                  className="comment__profile"
+                  src='/images/profile.png'
+                  alt='profile'
+                  className='comment__profile'
                 />
-                <p className="comment__text">
+                <p className='comment__text'>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
                   suscipit autem explicabo, ut ullam dignissimos tenetur
                   eligendi in velit, laborum, laudantium illo aut perferendis
                   animi. Eius iure quae quaerat in?
-                  <span className="comment__author">Author</span>
+                  <span className='comment__author'>Author</span>
                 </p>
               </div>
-              <div className="show__comment__container">
+              <div className='show__comment__container'>
                 <img
-                  src="/images/profile.png"
-                  alt="profile"
-                  className="comment__profile"
+                  src='/images/profile.png'
+                  alt='profile'
+                  className='comment__profile'
                 />
-                <p className="comment__text">
+                <p className='comment__text'>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
                   suscipit autem explicabo, ut ullam dignissimos tenetur
                   eligendi in velit, laborum, laudantium illo aut perferendis
                   animi. Eius iure quae quaerat in?
-                  <span className="comment__author">Author</span>
+                  <span className='comment__author'>Author</span>
                 </p>
               </div>
-              <div className="show__comment__container">
+              <div className='show__comment__container'>
                 <img
-                  src="/images/profile.png"
-                  alt="profile"
-                  className="comment__profile"
+                  src='/images/profile.png'
+                  alt='profile'
+                  className='comment__profile'
                 />
-                <p className="comment__text">
+                <p className='comment__text'>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
                   suscipit autem explicabo, ut ullam dignissimos tenetur
                   eligendi in velit, laborum, laudantium illo aut perferendis
                   animi. Eius iure quae quaerat in?
-                  <span className="comment__author">Author</span>
+                  <span className='comment__author'>Author</span>
                 </p>
               </div>
             </div>
